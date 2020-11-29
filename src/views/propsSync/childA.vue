06a16f7f0bb4ade6352634fae3c9ba91ab7d89c8
bulb <pumpkinjack@livemail.tw>
2020-11-29 20:51:32 +0800

    Add propsSync

---
+++ src/views/propsSync/childA.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <p>Count:{{count}}</p>
+        <button @click="childClick">Add by childA</button>
+
+        <p>Score:{{score}}</p>
+        <button @click="scoreClick">Calculate Score</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'childA',
+    props: {
+        score: Number,
+        count: Number
+    },
+    methods: {
+        childClick () {
+            console.log('click by childA')
+            this.$emit('counter-update', this.count + 1)
+        },
+        scoreClick () {
+            console.log('click by childA-score')
+            this.$emit('update:score', this.count * 10)
+        }
+    }
+}
+</script>
---
+++ src/views/propsSync/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <button @click="addCount" >ADD by parent</button>
+        <childA :count="count" @counter-update="addCount" :score.sync="score"/>
+    </div>
+</template>
+
+<script>
+import childA from './childA'
+
+export default {
+    name: 'propsSync',
+    components: {
+        childA
+    },
+    data () {
+        return {
+            count: 0,
+            score: 0
+        }
+    },
+    methods: {
+        addCount () {
+            this.count++
+        }
+    }
+}
+</script>
